13efbe8d900b24ad11b39a11a9f7abba5c34439b
Haoli Yin <haoli.yin@vanderbilt.edu>
2024-01-31 17:57:51 -0600

    Initial save

---
+++ run_training.py
@@ -0,0 +1,18 @@
+
+# Basic Libraries
+import os 
+import argparse
+import yaml
+
+# Deep Learning Libraries
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning import seed_everything 
+
+# set reproducible 
+torch.backends.cudnn.deterministc = True
+torch.backends.cudnn.benchmark = False
+torch.set_float32_matmul_precision('medium')
+
+seed = 0
+seed_everything(seed, workers=True)
