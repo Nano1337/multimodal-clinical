e83572cf4a742879d093e7c26fb8cc796134eccf
Haoli Yin <haoli.yin@vanderbilt.edu>
2024-03-20 17:37:15 -0500

    Make cremad noised ensemble compatible

--- cremad/ensemble_model_noised.py
+++ cremad/ensemble_model_noised.py
@@ -5,6 +5,7 @@ import torch.nn.functional as F
 import pytorch_lightning as pl
 
 from cremad.backbone import resnet18
+from utils.BaseModel import EnsembleBaseModel
 
 from torch.optim.lr_scheduler import StepLR
 
@@ -57,7 +58,7 @@ class FusionNet(nn.Module):
 
         return (x1_logits, x2_logits, x1_loss, x2_loss)
 
-class MultimodalCremadModel(pl.LightningModule): 
+class MultimodalCremadModel(EnsembleBaseModel): 
 
     def __init__(self, args): 
         """Initialize MultimodalCremadModel.
@@ -66,28 +67,12 @@ class MultimodalCremadModel(pl.LightningModule):
             args (argparse.Namespace): Arguments for the model        
         """
 
-        super(MultimodalCremadModel, self).__init__()
-
-        self.args = args
-        self.model = self._build_model()
+        super(MultimodalCremadModel, self).__init__(args)
 
         self.automatic_optimization = False
         self.ogm_modulation = self.args.grad_mod_type
         self.ogm_alpha = self.args.alpha
 
-        self.val_metrics = {
-            "val_loss": [], 
-            "val_acc": [],
-            "val_x1_acc": [], 
-            "val_x2_acc": [],
-        }
-
-        self.test_metrics = {
-            "test_loss": [], 
-            "test_acc": [], 
-            "test_x1_acc": [],
-            "test_x2_acc": [],
-        }
 
     def forward(self, x1, x2, label): 
         return self.model(x1, x2, label)
@@ -104,27 +89,31 @@ class MultimodalCremadModel(pl.LightningModule):
         
         """
 
-
         # Extract modality x1, modality x2, and label from batch
         x1, x2, label = batch
 
         # Get predictions and loss from model
         x1_logits, x2_logits, x1_loss, x2_loss = self.model(x1, x2, label)
 
-        # Calculate accuracy
-        x1_acc = torch.mean((torch.argmax(x1_logits, dim=1) == label).float())
-        x2_acc = torch.mean((torch.argmax(x2_logits, dim=1) == label).float())
+        # Calculate acc, unimodal acc not uncalibrated
+        x1_acc_cal = torch.mean((torch.argmax(x1_logits, dim=1) == label).float())
+        x2_acc_cal = torch.mean((torch.argmax(x2_logits, dim=1) == label).float())
         avg_logits = (x1_logits + x2_logits) / 2
         preds = torch.argmax(avg_logits, dim=1)
         joint_acc = torch.mean((torch.argmax(avg_logits, dim=1) == label).float())
         avg_loss = (x1_loss + x2_loss) / 2
 
         # Log loss and accuracy
-        self.log("train_loss", avg_loss, on_step=True, on_epoch=True, prog_bar=False, logger=True)
-        self.log("train_acc", joint_acc, on_step=True, on_epoch=True, prog_bar=False, logger=True)
+        self.log("train_step/train_loss", avg_loss, on_step=True, on_epoch=True, prog_bar=False, logger=True)
+        self.log("train_step/train_acc", joint_acc, on_step=True, on_epoch=True, prog_bar=False, logger=True)
+        self.log("train_step/train_x1_acc", x1_acc_cal, on_step=True, on_epoch=True, prog_bar=False, logger=True)
+        self.log("train_step/train_x2_acc", x2_acc_cal, on_step=True, on_epoch=True, prog_bar=False, logger=True)
 
-        # # Return the loss
-        # return avg_loss
+        # accumulate accuracies and losses
+        self.train_metrics["train_loss"].append(avg_loss)
+        self.train_metrics["train_acc"].append(joint_acc)
+        self.train_metrics["train_x1_acc"].append(x1_acc_cal)
+        self.train_metrics["train_x2_acc"].append(x2_acc_cal)
 
         opt = self.optimizers()
         opt.zero_grad()
@@ -133,7 +122,28 @@ class MultimodalCremadModel(pl.LightningModule):
             ogm_ge(self.model, x1_logits, x2_logits, label, modulation=self.ogm_modulation, alpha=self.ogm_alpha)
         opt.step()
 
-    def on_train_epoch_end(self):
+                # Return the loss
+        return avg_loss
+
+    def on_train_epoch_end(self) -> None:
+        """ Called at the end of the training epoch. Logs average loss and accuracy.
+
+        """
+        avg_loss = torch.stack(self.train_metrics["train_loss"]).mean()
+        avg_acc = torch.stack(self.train_metrics["train_acc"]).mean()
+        x1_acc = torch.mean(torch.stack(self.train_metrics["train_x1_acc"]))
+        x2_acc = torch.mean(torch.stack(self.train_metrics["train_x2_acc"]))
+
+        self.log("train_epoch/train_avg_loss", avg_loss, on_step=False, on_epoch=True, prog_bar=False, logger=True)
+        self.log("train_epoch/train_avg_acc", avg_acc, on_step=False, on_epoch=True, prog_bar=False, logger=True)
+        self.log("train_epoch/train_avg_x1_acc", x1_acc, on_step=False, on_epoch=True, prog_bar=False, logger=True)
+        self.log("train_epoch/train_avg_x2_acc", x2_acc, on_step=False, on_epoch=True, prog_bar=False, logger=True)
+
+        self.train_metrics["train_loss"].clear()
+        self.train_metrics["train_acc"].clear()
+        self.train_metrics["train_x1_acc"].clear()
+        self.train_metrics["train_x2_acc"].clear()
+
         if self.args.use_scheduler:
             schedulers = self.lr_schedulers()
             
@@ -142,134 +152,6 @@ class MultimodalCremadModel(pl.LightningModule):
                 schedulers = [schedulers]
             for scheduler in schedulers:
                 scheduler.step()
-
-    def validation_step(self, batch, batch_idx): 
-        """Validation step for the model. Logs loss and accuracy.
-
-        Args:
-            batch (Tuple[torch.Tensor, torch.Tensor, torch.Tensor]): Tuple containing screenshot, wireframe, and label
-            batch_idx (int): Index of the batch
-
-        Returns:
-            torch.Tensor: Loss
-
-        """
-
-        # Extract modality x1, modality x2, and label from batch
-        x1, x2, label = batch
-
-        # Get predictions and loss from model
-        x1_logits, x2_logits, x1_loss, x2_loss = self.model(x1, x2, label)
-
-        # Calculate accuracy
-        x1_acc = torch.mean((torch.argmax(x1_logits, dim=1) == label).float())
-        x2_acc = torch.mean((torch.argmax(x2_logits, dim=1) == label).float())
-        avg_logits = (x1_logits + x2_logits) / 2
-        joint_acc = torch.mean((torch.argmax(avg_logits, dim=1) == label).float())
-        avg_loss = (x1_loss + x2_loss) / 2
-
-        # Log loss and accuracy
-        self.log("val_loss", avg_loss, on_step=True, on_epoch=True, prog_bar=False, logger=True)
-        self.log("val_acc", joint_acc, on_step=True, on_epoch=True, prog_bar=False, logger=True)
-
-        self.val_metrics["val_loss"].append(avg_loss)
-        self.val_metrics["val_acc"].append(joint_acc)
-        self.val_metrics["val_x1_acc"].append(x1_acc)
-        self.val_metrics["val_x2_acc"].append(x2_acc)
-
-        # Return the loss
-        return avg_loss
-
-    def on_validation_epoch_end(self) -> None:
-        """ Called at the end of the validation epoch. Logs average loss and accuracy.
-
-        Applies unimodal offset correction to logits and calculates accuracy for each modality and jointly
-
-        """
-        avg_loss = torch.stack(self.val_metrics["val_loss"]).mean()
-        avg_acc = torch.stack(self.val_metrics["val_acc"]).mean()
-        x1_acc = torch.mean(torch.stack(self.val_metrics["val_x1_acc"]))
-        x2_acc = torch.mean(torch.stack(self.val_metrics["val_x2_acc"]))
-
-        self.log("val_loss", avg_loss, on_step=False, on_epoch=True, prog_bar=False, logger=True)
-        self.log("val_acc", avg_acc, on_step=False, on_epoch=True, prog_bar=False, logger=True)
-        self.log("x1_val_acc", x1_acc, on_step=False, on_epoch=True, prog_bar=False, logger=True)
-        self.log("x2_val_acc", x2_acc, on_step=False, on_epoch=True, prog_bar=False, logger=True)
-        
-        self.val_metrics["val_loss"].clear()
-        self.val_metrics["val_acc"].clear()
-        self.val_metrics["val_x1_acc"].clear()
-        self.val_metrics["val_x2_acc"].clear()
-
-    def test_step(self, batch, batch_idx):
-        """Test step for the model. Logs loss and accuracy.
-
-        Args:
-            batch (Tuple[torch.Tensor, torch.Tensor, torch.Tensor]): Tuple containing screenshot, wireframe, and label
-            batch_idx (int): Index of the batch
-
-        Returns:
-            torch.Tensor: Loss
-
-        """
-
-        # Extract modality x1, modality x2, and label from batch
-        x1, x2, label = batch
-
-        # Get predictions and loss from model
-        x1_logits, x2_logits, x1_loss, x2_loss = self.model(x1, x2, label)
-
-        # Calculate accuracy
-        x1_acc = torch.mean((torch.argmax(x1_logits, dim=1) == label).float())
-        x2_acc = torch.mean((torch.argmax(x2_logits, dim=1) == label).float())
-        avg_logits = (x1_logits + x2_logits) / 2
-        joint_acc = torch.mean((torch.argmax(avg_logits, dim=1) == label).float())
-        avg_loss = (x1_loss + x2_loss) / 2
-
-        # Log loss and accuracy
-        self.log("test_loss", avg_loss, on_step=True, on_epoch=True, prog_bar=False, logger=True)
-        self.log("test_acc", joint_acc, on_step=True, on_epoch=True, prog_bar=False, logger=True)
-
-        self.test_metrics["test_loss"].append(avg_loss)
-        self.test_metrics["test_acc"].append(joint_acc)
-        self.test_metrics["test_x1_acc"].append(x1_acc)
-        self.test_metrics["test_x2_acc"].append(x2_acc)
-
-        # Return the loss
-        return avg_loss
-    
-    def on_test_epoch_end(self):
-        """ Called at the end of the test epoch. Logs average loss and accuracy.
-
-        Applies unimodal offset correction to logits and calculates accuracy for each modality and jointly
-
-        """
-        avg_loss = torch.stack(self.test_metrics["test_loss"]).mean()
-        avg_acc = torch.stack(self.test_metrics["test_acc"]).mean()
-        x1_acc = torch.mean(torch.stack(self.test_metrics["test_x1_acc"]))
-        x2_acc = torch.mean(torch.stack(self.test_metrics["test_x2_acc"]))
-
-        self.log("test_loss", avg_loss, on_step=False, on_epoch=True, prog_bar=False, logger=True)
-        self.log("test_acc", avg_acc, on_step=False, on_epoch=True, prog_bar=False, logger=True)
-        self.log("x1_test_acc", x1_acc, on_step=False, on_epoch=True, prog_bar=False, logger=True)
-        self.log("x2_test_acc", x2_acc, on_step=False, on_epoch=True, prog_bar=False, logger=True)
-        
-        self.test_metrics["test_loss"].clear()
-        self.test_metrics["test_acc"].clear()
-        self.test_metrics["test_x1_acc"].clear()
-        self.test_metrics["test_x2_acc"].clear()
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.SGD(self.parameters(), lr=self.args.learning_rate, momentum=0.9, weight_decay=1.0e-4)
-        if self.args.use_scheduler:
-            scheduler = {
-                'scheduler': StepLR(optimizer, step_size=10, gamma=0.5),
-                'interval': 'epoch',
-                'frequency': 1,
-            }
-            return [optimizer], [scheduler]
-            
-        return optimizer
 
     def _build_model(self):
         return FusionNet(
